0247d7f6a398e122c93abbc5907bd34870fb4692
Dev_Akas5h <vishwakarmakash933@gmail.com>
2026-07-27 02:02:46 +0530

    Time: 17 ms (76.71%), Space: 47.4 MB (58.2%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+       int open =0;
+       StringBuilder first=new StringBuilder();
+       for(char ch:s.toCharArray()) {
+        if(ch=='('){
+            open++;
+            first.append(ch);
+            
+        }
+        else if(ch==')'){
+           if(open>0){
+            open--;
+            first.append(ch);
+           }
+           
+        }
+        else{
+            first.append(ch);
+        }
+       }
+       int close=0;
+       StringBuilder ans=new StringBuilder();
+       for(int i=first.length()-1;i>=0;i--) {
+        char ch=first.charAt(i);
+        if(ch==')'){
+            close++;
+            ans.append(ch);
+            
+        }
+        else if(ch=='('){
+           if(close>0){
+            close--;
+            ans.append(ch);
+           }
+           
+        }
+        else{
+            ans.append(ch);
+        }
+       }
+       return ans.reverse().toString();
+    }
+}
