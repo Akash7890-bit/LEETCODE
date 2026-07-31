058ed100152d8628e615aff0e6a526062f82f4e7
Dev_Akas5h <vishwakarmakash933@gmail.com>
2026-07-31 12:42:45 +0530

    Time: 0 ms (100%), Space: 44.2 MB (39.45%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>>resultList=new ArrayList<>();
+        List<Integer>temp=new ArrayList<>();
+         BT(resultList,temp,nums,0);
+         return resultList;
+    }
+    private void BT( List<List<Integer>>resultList,List<Integer>temp,int[]nums,int start){
+        resultList.add(new ArrayList<>(temp));
+
+        for(int i=start;i<nums.length;i++){
+            temp.add(nums[i]);
+            BT(resultList,temp,nums,i+1);
+            temp.remove(temp.size()-1);
+        }
+        
+    }
+}
