e69f048b5387348001f54cac706b54a16b336664
Dev_Akas5h <vishwakarmakash933@gmail.com>
2026-08-28 02:56:04 +0530

    Time: 3 ms (97.84%), Space: 46.6 MB (15.46%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,31 +14,29 @@
  * }
  */
 class Solution {
-    int ans=0;
     public int pathSum(TreeNode root, int targetSum) {
-        if(root==null){
+        
+        Map<Long, Integer> prefixCount = new HashMap<>();
+        prefixCount.put(0L, 1);
+
+        int count = dfs(root, targetSum, prefixCount, 0L);
+        return count;
+    }
+
+    private int dfs(TreeNode root, int targetSum, Map<Long, Integer> prefixCount, long prefixSum){
+        if(root == null){
             return 0;
         }
-        dfs(root,(long)targetSum);
-        pathSum(root.left,targetSum);
-        pathSum(root.right,targetSum);
-        return ans;
-       
-    }
-    public void dfs(TreeNode node,long target){
-        if(node==null){
-            return;
 
-        }
-        
-        if(target==node.val){
-            ans++;
-            
-            
-        }
-        dfs(node.left,target-node.val);
-        dfs(node.right,target-node.val);
-        
-        
+        prefixSum += root.val;
+        int count = prefixCount.getOrDefault(prefixSum - targetSum, 0);
+        prefixCount.put(prefixSum, prefixCount.getOrDefault(prefixSum, 0) + 1);
+
+        count += dfs(root.left, targetSum, prefixCount, prefixSum);
+        count += dfs(root.right, targetSum, prefixCount, prefixSum);
+
+        prefixCount.put(prefixSum, prefixCount.get(prefixSum) - 1);
+
+        return count;
     }
 }
