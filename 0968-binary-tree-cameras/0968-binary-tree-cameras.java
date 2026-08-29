7304b48aa1b9434e292d9bd28b76ea8fdaefff99
Dev_Akas5h <vishwakarmakash933@gmail.com>
2026-08-29 14:13:50 +0530

    Time: 0 ms (100%), Space: 44.8 MB (30.11%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.java
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -16,28 +16,28 @@
 class Solution {
      int ans=0;
     public int minCameraCover(TreeNode root) {
-        
-       int res[]=find(root);
-       if(res[0]==0 && res[1]==1){
-       ans++;
-       }
+        int res=find(root);
+        if(res==-1){
+            ans++;
+        }
+       
        return ans;
     }
-    public int[]find(TreeNode node){
+    public int find(TreeNode node){
         if(node==null){
-            return new int[]{0,0};
+            return 0;
         }
-        int []L=find(node.left);
-        int R[]=find(node.right);
-        if(L[1]==1 || R[1]==1){
+        int L=find(node.left);
+        int R=find(node.right);
+        if(L==-1 || R==-1){
             ans++;
-            return new int[]{1,0};
+            return 1;
         }
-        else if(L[0]==0 && R[0]==0){
-            return new int[]{0,1};
+        else if(L==1 || R==1){
+            return 0;
         }
         else{
-            return new int[]{0,0};
+            return -1;
         }
     }
 }
